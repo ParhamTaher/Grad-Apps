8a64b91a009c8f9413b0355ae13e3677fc101bfe
Teshome Alex <alex_teshome@hotmail.com>
2018-03-22 18:48:08 -0400

    Added notes and button to approve the ticket, button does not work yet

--- client/src/components/TicketCard.js
+++ client/src/components/TicketCard.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Panel } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
-const TicketCard = ({ TID, applicant, ticketStatus, ticketType }) => {
-    return (
-        <Panel eventKey={TID}>
-            <Panel.Heading>
-                <Panel.Title toggle>{applicant}</Panel.Title>
-            </Panel.Heading>
-            <Panel.Body collapsible>Ticket Status: {ticketStatus}</Panel.Body>
-            <Panel.Body collapsible>Ticket Type: {ticketType}</Panel.Body>
-        </Panel>
-    );
-};
+class TicketCard extends React.Component {
+	handleClick() {
+		this.props.onApproveSubmit(this.props.TID);
+	}
+
+    render() {
+	    return (
+	        <Panel eventKey={this.props.TID}>
+	            <Panel.Heading>
+	                <Panel.Title toggle>{this.props.applicant}</Panel.Title>
+	            </Panel.Heading>
+	            <Panel.Body collapsible>Ticket Status: {this.props.ticketStatus}</Panel.Body>
+	            <Panel.Body collapsible>Ticket Type: {this.props.ticketType}</Panel.Body>
+	            <Panel.Body collapsible><textarea>{this.props.notes}</textarea></Panel.Body>
+	            <Panel.Body collapsible><Button bsStyle="primary" onClick = {this.handleClick}>Primary</Button></Panel.Body>
+
+	        </Panel>
+	    );
+	}
+}
 
 export default TicketCard;
--- client/src/containers/ACDashboard.js
+++ client/src/containers/ACDashboard.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as Actions from '../actions';
+import { bindActionCreators } from 'redux';
 import ItemList from '../components/ItemList';
+import { Button } from 'react-bootstrap';
 
 class ACDashboard extends Component {
+    componentDidMount() {
+        console.log('Requesting tickets...');
+        this.props.actions.requestTickets(21);
+    }
+
     render() {
         return (
-            <div>
-                <h4>AC Dashboard</h4>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h3>Associate Chair Dashboard</h3>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6 col-sm-push-6">
+                        <h4>Domestic Tickets</h4>
+                        <ItemList
+                            ticketList={this.props.ticketList.tickets}
+                            ticketType={'D'}
+                            onApproveSubmit={this.props.actions.approveApplicant}
+                        />
+                    </div>
+                    <div className="col-sm-6 col-sm-pull-6">
+                        <h4>International Tickets</h4>
+                        <ItemList
+                            ticketList={this.props.ticketList.tickets}
+                            ticketType={'I'}
+                           	onApproveSubmit={this.props.actions.approveApplicant}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <input type="file" onChange={this.handleFileUpload} />
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default ACDashboard;
+function mapStateToProps(state) {
+    // Whatever is returned will show up as props
+    return {
+        ticketList: state.tickets,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(Actions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ACDashboard);
