110c6846904abdd0fe4478e773d93a40dbdc3327
Parham Taher <parham.taher@gmail.com>
2018-03-23 16:13:43 -0400

    small changes

--- client/src/components/TicketCard.js
+++ client/src/components/TicketCard.js
@@ -3,24 +3,33 @@ import { Panel } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
 class TicketCard extends React.Component {
-	handleClick() {
-		this.props.onApproveSubmit(this.props.TID);
-	}
+    handleClick() {
+        this.props.onApproveSubmit(this.props.TID);
+    }
 
     render() {
-	    return (
-	        <Panel eventKey={this.props.TID}>
-	            <Panel.Heading>
-	                <Panel.Title toggle>{this.props.applicant}</Panel.Title>
-	            </Panel.Heading>
-	            <Panel.Body collapsible>Ticket Status: {this.props.ticketStatus}</Panel.Body>
-	            <Panel.Body collapsible>Ticket Type: {this.props.ticketType}</Panel.Body>
-	            <Panel.Body collapsible><textarea>{this.props.notes}</textarea></Panel.Body>
-	            <Panel.Body collapsible><Button bsStyle="primary" onClick = {this.handleClick}>Primary</Button></Panel.Body>
-
-	        </Panel>
-	    );
-	}
+        return (
+            <Panel eventKey={this.props.TID}>
+                <Panel.Heading>
+                    <Panel.Title toggle>{this.props.applicant}</Panel.Title>
+                </Panel.Heading>
+                <Panel.Body collapsible>
+                    Ticket Status: {this.props.ticketStatus}
+                </Panel.Body>
+                <Panel.Body collapsible>
+                    Ticket Type: {this.props.ticketType}
+                </Panel.Body>
+                <Panel.Body collapsible>
+                    <textarea>{this.props.notes}</textarea>
+                </Panel.Body>
+                <Panel.Body collapsible>
+                    <Button bsStyle="primary" onClick={this.handleClick}>
+                        Submit
+                    </Button>
+                </Panel.Body>
+            </Panel>
+        );
+    }
 }
 
 export default TicketCard;
