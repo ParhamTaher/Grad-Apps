0044f8c896822ff071ba17b903cb311379a7f665
Parham Taher <parham.taher@gmail.com>
2018-04-02 18:27:51 -0400

    Setup concurrently for all 3 servers

--- server/gapf/gapf.js
+++ server/gapf/gapf.js
@@ -6,16 +6,16 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const Grid = require('gridfs-stream');
 const fs = require('fs');
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
 
 mongoose.connect(
-	'mongodb://proj-team13:' +
-	'team13db' +
-	'@node-rest-gradapp-shard-00-00-imzxv.mongodb.net:27017,' +
-	'node-rest-gradapp-shard-00-01-imzxv.mongodb.net:27017,' +
-	'node-rest-gradapp-shard-00-02-imzxv.mongodb.net:27017/' +
-	'test?ssl=true&replicaSet=node-rest-gradapp-shard-0&authSource=admin'
-	);
+    'mongodb://proj-team13:' +
+        'team13db' +
+        '@node-rest-gradapp-shard-00-00-imzxv.mongodb.net:27017,' +
+        'node-rest-gradapp-shard-00-01-imzxv.mongodb.net:27017,' +
+        'node-rest-gradapp-shard-00-02-imzxv.mongodb.net:27017/' +
+        'test?ssl=true&replicaSet=node-rest-gradapp-shard-0&authSource=admin'
+);
 
 mongoose.Promise = global.Promise;
 
@@ -28,11 +28,11 @@ app.use(morgan('dev'));
 app.use(busboy());
 app.use(gapfRoutes);
 
-app.use(function(err, req, res, next){
-  //console.log(err);
-  res.send({error: err.message});
+app.use(function(err, req, res, next) {
+    //console.log(err);
+    res.send({ error: err.message });
 });
 
-app.listen(port, function(){
-  console.log("now listening for requests on port '%d'", port);
+app.listen(port, function() {
+    console.log("now listening for requests on port '%d'", port);
 });
