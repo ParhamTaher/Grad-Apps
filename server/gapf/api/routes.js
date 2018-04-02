13da71962594633d6e14740e8fe240e4a6c774fd
tritran23 <trimeister.tran@mail.utoronto.ca>
2018-04-02 04:35:48 -0400

    cleaned up code, files are downloaded properly now

--- server/gapf/api/routes.js
+++ server/gapf/api/routes.js
@@ -1,42 +1,62 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 const router = express.Router();
 const GapfController = require('./controllers/gapfController');
 const Grid = require("gridfs-stream");
-//const conn = require('../../db');
-//mongoose.connect('mongodb://localhost/gapf');
 const conn = mongoose.connection;
 Grid.mongo = mongoose.mongo;
-let gfs;
 
+let gfs;
 conn.once('open', () => {
   console.info('Mongo db connected successfully');
   gfs = Grid(conn.db);
+  gfs.collection('Gapfs');
   //get GAPF
-  router.get('/gapf/:gapf_id', (req, res, next) => {
-    let data = "";
-    console.log(req.params.gapf_id);
-    let readstream = gfs.createReadStream({
-      _id: req.params.gapf_id,
-      //need to find way for metadata.faculty_id
-      root: 'Gapfs',
-    });
 
-    readstream.on('data', (chunk) => {
-      data += chunk;
-    });
+  router.get('/gapf', (req, res, next) => {
+    //change to request session later
+    gfs.files.find({'metadata.faculty_id' : '12'}).toArray((err, files) =>{
 
-    readstream.on('end', () => {
-      // res.status(200).send({
-      //   'gapf_id': req.params.gapf_id,
-      //   'file': data
-      // })
-      console.log(data);
-    });
+      if (!files || files.length === 0){
+        return res.status(404).json({
+          err: 'No files exist'
+        });
+      }
 
-    readstream.on('error', (err) => {
-      res.status(500).send(err);
-      console.log(err);
+      res.json(files);
+    });
+  });
+
+  router.get('/gapf/:gapf_id/download', (req, res, next) => {
+    const gapf_id = req.params.gapf_id;
+    gfs.files.findOne({
+      _id : new ObjectId(req.params.gapf_id)},
+      (err, file) =>{
+
+      if (!file || file.length === 0){
+        return res.status(404).json({
+          err: 'No file exist'
+        });
+      }
+
+      console.log(file._id);
+
+      let readstream = gfs.createReadStream({
+        _id: file._id,
+        //need to find way for metadata.faculty_id
+        root: 'Gapfs',
+      });
+
+      res.set('gapf_id', file._id);
+      res.set('Content-Disposition', 'attachment; filename="' + file.filename + '"');
+      res.set('Content-Type', file.contentType);
+      readstream.pipe(res);
+
+      readstream.on('error', (err) => {
+        res.status(500).send(err);
+        console.log(err);
+      });
     });
   });
 
@@ -47,6 +67,7 @@ conn.once('open', () => {
       filename: part.name,
       root: 'Gapfs',
       mode: 'w',
+      content_type: part.mimetype,
       metadata:{
         faculty_id: req.body.faculty_id
       }
@@ -67,11 +88,8 @@ conn.once('open', () => {
     });
   });
 
-  //update GAPF
-  router.put('/gapf/attach',GapfController.update);
-
   //delete GAPF
-  router.delete('/gapf/:gapf_id', (req, res, next) => {
+  router.delete('/gapf/:gapf_id/delete', (req, res, next) => {
     console.log(req.params.gapf_id);
     gfs.remove({
       _id: req.params.gapf_id,
