5976c76fb81ebcc256d87566c66d008727f4fb24
tritran23 <trimeister.tran@mail.utoronto.ca>
2018-04-04 23:57:49 -0400

    file upload with authentication

--- server/gapf/api/routes.js
+++ server/gapf/api/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 const router = express.Router();
-const GapfController = require('./controllers/gapfController');
+// const GapfController = require('./controllers/gapfController');
 const Grid = require("gridfs-stream");
 const conn = mongoose.connection;
 Grid.mongo = mongoose.mongo;
@@ -15,21 +15,39 @@ conn.once('open', () => {
   //get GAPF
 
   router.get('/gapf', (req, res, next) => {
-    //change to request session later
-    gfs.files.find({'metadata.faculty_id' : '12'}).toArray((err, files) =>{
+    //if faculty, get only files associated with that faculty
+    if (req.session.role == 'Faculty') {
+      gfs.files.find({'metadata.userId' : req.session.userId}).toArray((err, files) =>{
 
-      if (!files || files.length === 0){
-        return res.status(404).json({
-          err: 'No files exist'
-        });
-      }
+        if (!files || files.length === 0){
+          return res.status(404).json({
+            err: 'No files exist'
+          });
+        }
 
-      res.json(files);
-    });
+        res.json(files);
+      });
+    } //else get all Gapf's if its FSS or budget director
+      else if ((req.session.role == 'FSS') || (req.session.role == 'Budget Director')){
+      gfs.files.find().toArray((err, files) =>{
+
+        if (!files || files.length === 0){
+          return res.status(404).json({
+            err: 'No files exist'
+          });
+        }
+
+        res.json(files);
+      });
+    } else {
+      res.status(401).json({
+        message: 'Unauthorized User*'
+      });
+    }
   });
 
   router.get('/gapf/:gapf_id/download', (req, res, next) => {
-    const gapf_id = req.params.gapf_id;
+    //const gapf_id = req.params.gapf_id;
     gfs.files.findOne({
       _id : new ObjectId(req.params.gapf_id)},
       (err, file) =>{
@@ -40,11 +58,10 @@ conn.once('open', () => {
         });
       }
 
-      console.log(file._id);
+      //console.log(file._id);
 
       let readstream = gfs.createReadStream({
         _id: file._id,
-        //need to find way for metadata.faculty_id
         root: 'Gapfs',
       });
 
@@ -62,48 +79,87 @@ conn.once('open', () => {
 
   //new GAPF
   router.post('/gapf/attach', (req, res, next) => {
-    let part = req.files.file;
-    let writeStream = gfs.createWriteStream({
-      filename: part.name,
-      root: 'Gapfs',
-      mode: 'w',
-      content_type: part.mimetype,
-      metadata:{
-        faculty_id: req.body.faculty_id
-      }
-    });
-
-    writeStream.on('close', (file) => {
-      if(!file) {
-        res.status(400).send('No file received');
-      }
-      return res.status(200).send({
-          message: 'Success',
-          file: file
+    let userId = req.body.userId;
+    //check the request userid passed in equals to logged in use and if they're faculty or fss
+    if (userId === req.session.userId && (req.session.role === 'Faculty' || req.session.role === 'FSS')){
+      let part = req.files.file;
+      let writeStream = gfs.createWriteStream({
+        filename: part.name,
+        root: 'Gapfs',
+        mode: 'w',
+        content_type: part.mimetype,
+        metadata:{
+          userId: userId,
+          role: req.session.role
+        }
       });
-    });
 
-    writeStream.write(part.data, () => {
-      writeStream.end();
-    });
+      writeStream.on('close', (file) => {
+        if(!file) {
+          res.status(400).send('No file received');
+        }
+        return res.status(200).send({
+            message: 'Success',
+            file: file
+        });
+      });
+
+      writeStream.write(part.data, () => {
+        writeStream.end();
+      });
+      } else {
+      res.status(401).json({
+        message: 'Unauthorized User*'
+      });
+    }
   });
 
   //delete GAPF
   router.delete('/gapf/:gapf_id/delete', (req, res, next) => {
-    console.log(req.params.gapf_id);
-    gfs.remove({
-      _id: req.params.gapf_id,
-      root: 'Gapfs'
-    }, (err, gridStore) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
-      return res.status(200).send({
-          message: 'Success',
-          file_name: req.params.gapf_id
-      });
+    //console.log(req.params.gapf_id);
+    //only faculty can delete their own file
+    if (req.session.role === 'Faculty'){
+      //check if the file is created by user trying to delete it
+      gfs.exist({
+        _id : new ObjectId(req.params.gapf_id),
+        'metadata.userId': req.session.userId,
+        root: 'Gapfs'
+      }, (err, found) =>{
+
+        if (err) {
+          return res.status(500).json({
+            message: 'error checking if file exists in delete request'
+          })
+        }
+
+        if (found) {
+          gfs.remove({
+            _id: req.params.gapf_id,
+            root: 'Gapfs'
+          }, (err, gridStore) => {
+            if (err) {
+              return res.status(500).json({
+                message: 'error removing file'
+              });
+            }
+            return res.status(200).send({
+                message: 'Success',
+                fileId: req.params.gapf_id
+              });
+          });
+        } else{
+          return res.status(404).json({
+            err: 'Cant delete because the file doesnt exist or not authorized to delete'
+          });
+        }
     });
+    } else {
+      res.status(401).json({
+        message: 'Unauthorized User: delete'
+      });
+    }
   });
 });
+
 
 module.exports = router;
